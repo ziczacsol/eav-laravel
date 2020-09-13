77013ec6c7678871a40b90979dd104990345703d
hieutran1301 <tranhieu130196@gmail.com>
2020-09-13 16:17:36 +0700

    update composer

---
+++ src/EavServiceProvider.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Eav;
+
+use Illuminate\Support\ServiceProvider;
+// use App\Eav\EntityObserve;
+// use App\Eav\EavModel;
+// use App\Eav\GlobalScope;
+
+class EavServiceProvider extends ServiceProvider
+{
+    /**
+     * Command mappings for the application
+     *
+     * @var array
+     */
+    protected $commands = [
+        'App\Eav\Commands\MakeEntity',
+        'App\Eav\Commands\MakeEntityModel',
+        'App\Eav\Commands\MakeEntityMigration',
+    ];
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        // $this->app->singleton(GlobalScope::class, function ($app) {
+        //     return new GlobalScope(new EavModel());
+        // });
+        // $this->app->singleton(EntityObserve::class, function ($app) {
+        //     return new EntityObserve(new EavModel(), $app['events']);
+        // });
+
+        $this->commands($this->commands);
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+
+    }
+}
